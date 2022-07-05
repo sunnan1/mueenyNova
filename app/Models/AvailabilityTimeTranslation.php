3b89aff1f381f75aa964a911789cc194135043a0
sunnan1 <sunnan.fazal95@hotmail.com>
2022-07-05 17:54:54 +0500

    Availability Time

--- app/Models/AvailabilityTimeTranslation.php
+++ app/Models/AvailabilityTimeTranslation.php
@@ -3,7 +3,11 @@
 namespace App\Models;
 
 class AvailabilityTimeTranslation extends MyModel {
-    
+
     protected $table = "availability_time_translations";
 
+    public function avaibilityTime()
+    {
+        return $this->belongsTo(AvailabilityTime::class , 'availability_time_id' , 'id');
+    }
 }
