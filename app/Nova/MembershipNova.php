7d31eb63e9bb602f15e2c755e162900168f69ca0
sunnan1 <sunnan.fazal95@hotmail.com>
2022-07-17 11:13:47 +0500

    membership nova

---
+++ app/Nova/MembershipNova.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Nova;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Laravel\Nova\Fields\Avatar;
+use Laravel\Nova\Fields\BelongsTo;
+use Laravel\Nova\Fields\Boolean;
+use Laravel\Nova\Fields\Currency;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Number;
+use Laravel\Nova\Fields\Select;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class MembershipNova extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static $model = \App\Models\MembershipNova::class;
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'name_en';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'id',
+    ];
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function fields(Request $request)
+    {
+        return [
+            ID::make(__('ID'), 'id')->sortable(),
+            Avatar::make('Image', 'image')
+                ->disk('public')
+                ->resolveUsing(fn ($v) => $v ?: '')
+                ->store(function (Request $request, \App\Models\MembershipNova $model) {
+                    if ($model->image) {
+                        Storage::disk('public')->delete($model->image);
+                    }
+                    return ['image' => $request->image->store('/uploads', 'public')];
+                })
+                ->disableDownload(),
+            Text::make('Name EN' , 'name_en'),
+            Text::make('Name AR' , 'name_ar'),
+            Boolean::make('Active' , "is_active")
+                ->trueValue(1)
+                ->falseValue(0),
+            Number::make('Points' , 'points'),
+            Text::make('Amount' , 'amount'),
+            Select::make('Membership Type' , 'membership_type')->options([
+                'admins' => 'Admins',
+                'stores' => 'Stores'
+            ]),
+            Text::make('Commission' , 'commission_percentage'),
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function cards(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function filters(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function lenses(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function actions(Request $request)
+    {
+        return [];
+    }
+}
